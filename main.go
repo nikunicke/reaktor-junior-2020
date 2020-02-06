6aa0849644b2969bf28ec6701040dfbdc423efab
root <pimenoff.niklas@gmail.com>
2020-02-06 22:15:48 +0000

    Testing git submodule features

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "fmt"
 	"log"
 	"net/http"
 	"io/ioutil"
@@ -15,46 +14,26 @@ type Package struct {
 	Description		string
 }
 
-func dpkg_all(w http.ResponseWriter, r *http.Request) {
+func get_dpkg_data(w http.ResponseWriter, r *http.Request) {
+	var packages []Package
+
 	data, err := ioutil.ReadFile("/root/data/dpkg/dpkg.json")
 	if err != nil {
-		log.Println("Error fetching file")
+		http.Error(w, err.Error(), 500)
 		return
 	}
-	var packages []Package
-	err = json.Unmarshal(data, &packages)
-	if err != nil {
-		log.Println(err)
+	if err = json.Unmarshal(data, &packages); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(packages)
 }
 
-// func dpkg_one(w http.ResponseWriter, r *http.Request) {
-// 	data, err := ioutil.ReadFile("/root/data/dpkg/dpkg.json")
-// 	if err != nil {
-// 		log.Println("Error fetching file")
-// 		return
-// 	}
-// 	var packages []Package
-// 	err = json.Unmarshal(data, &packages)
-// 	if err != nil {
-// 		log.Println(err)
-// 	}
-// 	title := r.URL.Path[len("/package/"):]
-// 	for _, pack := range packages {
-// 		if title == pack.Package {
-// 			json.NewEncoder(w).Encode(pack)
-// 			return
-// 		}
-// 	}
-// }
-
 func main() {
 	path := "/root/go/go_services/reaktor-app/static/build"
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(path))))
-	http.HandleFunc("/api/dpkg/", dpkg_all)
-	// http.HandleFunc("/package/", dpkg_one)
+	http.HandleFunc("/api/dpkg/", get_dpkg_data)
 	log.Println("Server running on port: 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
