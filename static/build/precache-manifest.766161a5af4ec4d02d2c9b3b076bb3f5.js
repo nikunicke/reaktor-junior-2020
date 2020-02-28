4cb294f4367f519fb0c9ac54765f068619968415
root <pimenoff.niklas@gmail.com>
2020-02-28 15:42:46 +0000

    adding static frontend here so program can be ran without building with npm

---
+++ static/build/precache-manifest.766161a5af4ec4d02d2c9b3b076bb3f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be627cc099e2f83d36aff2ca1d66986",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06943731eb68a5c31292",
+    "url": "/static/css/main.ce5a4348.chunk.css"
+  },
+  {
+    "revision": "1f058ae318130965302e",
+    "url": "/static/js/2.875710de.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.875710de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06943731eb68a5c31292",
+    "url": "/static/js/main.bd037246.chunk.js"
+  },
+  {
+    "revision": "1fc93c1b64693ae53552",
+    "url": "/static/js/runtime-main.4ee4355f.js"
+  }
+]);
